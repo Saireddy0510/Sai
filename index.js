7d7bd2fca125c22429b117cb52b24269385358d4
Saireddy0510 <sainathreddy0510@gmail.com>
2024-09-04 16:41:40 +0530

    sai

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+function Bigmessage(){
+    console.log("sai")
+}
 setTimeout(()=> {
-    console.log("Hello")
+    console.log("Sai")
 },5000)
 const interval = setInterval(() => {
-    console.log("Hello")
+    console.log("Sai")
 },10000);
 setTimeout(()=> {
     console.log("Stop")
